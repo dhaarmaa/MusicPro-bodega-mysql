aaf79de396e409a432f42c1646b064bba393e043
mgonzalezll1464@gmail.com <mgonzalezll1415@gmail.com>
2023-06-03 22:39:42 -0400

    frontend registro

--- frontend/src/views/login.js
+++ frontend/src/views/login.js
@@ -7,16 +7,20 @@ const Login = () => {
             <div>
                 <div className="login-box">
                     <img className='avatar' src="../logoWhite.svg" alt="Logo MusicPro" />
-                    <h1>Login Bodega MusicPro</h1>
-                    <form action="">
+                    <h1>Login Bodega</h1>
+                    <form>
                         {/* //Username */}
                         <label for="username">Correo</label>
-                        <input type="text" placeholder="Ingrese su correo"/>
+                        <input type="text" placeholder="Ingrese su correo" required/>
+                        <div class="invalid-feedback">
+                            correo invalido
+                        </div>  
 
                         {/* //Password */}
                         <label for="password">Contraseña</label>
-                        <input type="password" placeholder="Ingrese su contraseña"/>
+                        <input type="password" placeholder="Ingrese su contraseña" required/>
 
+                        {/* boton de login */}
                         <input type="submit" value="Ingresar"/>
                     </form>
                 </div>    
--- frontend/src/views/register.js
+++ frontend/src/views/register.js
@@ -1,69 +1,56 @@
 import React from 'react';
-import '../register.css'
+import '../register.css';
 
 
 const Register = () => {
 
     
     return(
-
       <div class="register">
-        <div>
-          <img src="logoBlack.svg" alt="" width="150"/>
-          <h1>Registro</h1>
-        </div>
-        <form class="column g-3 needs-validation" novalidate>
+          <img className= 'logo' src="../logoWhite.svg" alt="" width="150"/>
+          <h1>Registro Bodega</h1>
+        
+        <form>
           {/* ingreso del nombre */}
-          <div class="col-md-3 .offset-md-3">
             <label for="validationName" class="form-label">Nombre</label>
-            <input type="text" class="form-control" id="validationName"  required/>        
-          </div>
-
+            <input type="text" class="form-control" placeholder='Ingrese su nombre' id="validationName"  required/>        
+          
           {/* ingreso del apellido */}
-          <div class="col-md-4">
             <label for="validationLastName" class="form-label">Apellido</label>
-            <input type="text" class="form-control" id="validationCustom02"  required/>         
-          </div>
+            <input type="text" class="form-control" placeholder='Ingrese su apellido' id="validationCustom02"  required/>         
 
           {/* ingreso del correo */}
-          <div class="col-md-4">
             <label for="validationCustom03" class="form-label">Correo</label>
-            <input type="email" class="form-control" id="validationCustom03" required/>
+            <input type="email" class="form-control" placeholder='Ingrese su correo' id="validationCustom03" required/>
             <div class="invalid-feedback">
               correo invalido
             </div>
-          </div>
-
+          
           {/* ingreso de la contraseña */}
-          <div class="col-md-4">
             <label for="validationCustom03" class="form-label">Contraseña</label>
-            <input type="password" class="form-control" id="validationCustom03" required/>
+            <input type="password" class="form-control" placeholder='Ingrese su contraseña' id="validationCustom03" required/>
             <div class="invalid-feedback">
               contraseña invalido
             </div>
-          </div>
-
+          
           {/* ingreso de confirmación de contraseña */}
-          <div class="col-md-4">
-            <label for="validationCustom03" class="form-label">Confirmación de ontraseña</label>
-            <input type="password" class="form-control" id="validationCustom03" required/>
+            <label for="validationCustom03" class="form-label">Confirmación de contraseña</label>
+            <input type="password" class="form-control" placeholder='Confirme su contraseña' id="validationCustom03" required/>
             <div class="invalid-feedback">
               contraseña invalido
             </div>
-          </div>
+          
 
           {/* ingreso de fecha de nacimiento */}
-          <div class="col-md-3">
             <label for="validationCustom05" class="form-label">Fecha de nacimiento</label>
             <input type="date" class="form-control" id="validationCustom05" required/>       
-          </div>
+
 
           {/* boton de registro */}
-          <div class="col-12">
-            <button class="btn btn-primary" type="submit">Registrarse</button>
-          </div>
+            <input type="submit" value="Registrarme"/>
         </form>
       </div>
+   
     )   
 }
 
